419b560177cb00170213eb267e195e12f9411d7a
acidcode <acidkp@gmail.com>
2023-01-02 20:11:53 -0300

    testes unitarios

---
+++ tests/AppTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests\Unit\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+class AppTest extends TestCase {
+
+    public function testAlgo() {
+
+        $this->assertEquals(2,2);
+    }
+
+
+}
